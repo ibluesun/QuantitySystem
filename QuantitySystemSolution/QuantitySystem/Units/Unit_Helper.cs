50dbe7f9b5d4befa6ba90ff29f80703fb59e908d
IBlueSun_cp <SND\IBlueSun_cp>
2009-04-28 08:08:17 +0000

    Ensuring every thing is checked in with some improvements in code. still in testing phase.

---
+++ QuantitySystemSolution/QuantitySystem/Units/Unit_Helper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuantitySystem.Quantities.BaseQuantities;
+
+namespace QuantitySystem.Units
+{
+    public partial class Unit
+    {
+        
+        /// <summary>
+        /// Returns quantity based on current unit instance.
+        /// </summary>
+        /// <typeparam name="T">Quatntity Storage Type</typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        internal AnyQuantity<T> MakeQuantity<T>(T value)
+        {
+
+
+            //create the corresponding quantity
+            AnyQuantity<T> qty = this.GetThisUnitQuantity<T>();
+
+            //assign the unit to the created quantity
+            qty.Unit = this;
+
+            //assign the value to the quantity
+            qty.Value = value;
+
+            return qty;
+
+        }
+
+        
+        
+
+        /// <summary>
+        /// Returns quantity from the current unit type.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static AnyQuantity<double> QuantityOf<TUnit>(double value) where TUnit:Unit, new()
+        {
+
+            Unit unit = new TUnit();
+            return unit.MakeQuantity<double>(value);
+
+        }
+    }
+}
