7efc1d23b08bdd61a77e57732522adacd380ed9c
IBlueSun_cp <SND\IBlueSun_cp>
2009-07-19 09:03:39 +0000

    Facrorial enhanced to include real numbers.

--- QuantitySystemSolution/QuantitySystem.Runtime/QsTypes/Gamma.cs
+++ QuantitySystemSolution/QuantitySystem.Runtime/QsTypes/Gamma.cs
@@ -15,14 +15,13 @@ namespace Qs.QsTypes
     public static class Gamma
     {
         
-        public static AnyQuantity<double> Factorial(AnyQuantity<double> Number)
+        public static AnyQuantity<double> Factorial(AnyQuantity<double> number)
         {
-            
-            int v = (int)Number.Value;
+            int v = (int)number.Value;
 
             if (v > 170) throw new NotImplementedException("Number Exceededs Factorial Limit > 170");
 
-            AnyQuantity<double> num = (AnyQuantity<double>)Number.Clone();
+            AnyQuantity<double> num = (AnyQuantity<double>)number.Clone();
             num.Value = Math.Floor(num.Value);
 
             AnyQuantity<double> Total = (AnyQuantity<double>)num.Clone();
@@ -33,8 +32,75 @@ namespace Qs.QsTypes
                 Total = Total * num;
             }
 
+            if (number.Value > v) Total.Value = GammaFactorial(number.Value);
+
             return Total;
         
+        }
+
+        public static double GammaFactorial(double number)
+        {
+            //I checked this http://www.rskey.org/gamma.htm 
+            // then this ...  http://www.luschny.de/math/factorial/approx/SimpleCases.html
+
+
+
+            //the used function  near to the windows calculator
+            Func<double, double> StieltjesFactorial = delegate(double x)
+            {
+                var y = x + 1; var p = 1.0;
+                while (y < 7) { p = p * y; y = y + 1; }
+                var r = Math.Exp(y * Math.Log(y) - y + 1 / (12 * y + 2 / (5 * y + 53 / (42 * y))));
+                if (x < 7) r = r / p;
+                return r * Math.Sqrt(2 * Math.PI / y);
+            };
+
+
+            Func<double, double> StieltjesLnFactorial = delegate(double z)
+            {
+                var a0 = 1 / 12; var a1 = 1 / 30; var a2 = 53 / 210; var a3 = 195 / 371;
+                var a4 = 22999 / 22737; var a5 = 29944523 / 19733142;
+                var a6 = 109535241009 / 48264275462;
+                var Z = z + 1;
+
+                return (1 / 2) * Math.Log(2 * Math.PI) + (Z - 1 / 2) * Math.Log(Z) - Z +
+                    a0 / (Z + a1 / (Z + a2 / (Z + a3 / (Z + a4 / (Z + a5 / (Z + a6 / Z))))));
+            };
+
+            //gives value of the normal factorial :S
+            Func<double, double> StieltjesFactorialHP = delegate(double x)
+            {
+                var y = x; var p = 1.0;
+                while (y < 8)  
+                {
+                    p = p*y;
+                    y = y+1;
+                };
+                var r = Math.Exp(StieltjesLnFactorial(y));
+
+                if (x < 8) return  (r * x) / (p * y);
+                else
+                    return r;
+            };
+
+
+            //gives value less the normal factorial :(
+            Func<double, double> Nemes = delegate(double x)
+            {
+                double temp = x;
+                temp += (1 / 12) * (1 / x);
+                temp += (1 / 1440) * (1 / x * x * x);
+                temp += (239 / 362880) * (1 / x * x * x * x * x);
+
+                return Math.Pow(temp, x) * Math.Exp(-x) * Math.Sqrt(2) * Math.Sqrt(Math.PI * x);
+
+            };
+
+            
+            return StieltjesFactorial(number);
+            
+
+
         }
 
     }
