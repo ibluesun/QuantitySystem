97657d54f94a1ad383add9e7ba6d72f89cb98ff8
starnutoditopo_cp <SND\starnutoditopo_cp>
2010-07-29 08:42:26 +0000

    Commented the overridden method ToString in DeviceIndependentPixel

--- QuantitySystemSolution/QuantitySystem/Units/Misc/LengthUnits.cs
+++ QuantitySystemSolution/QuantitySystem/Units/Misc/LengthUnits.cs
@@ -24,15 +24,15 @@ namespace QuantitySystem.Units.Misc
     [ReferenceUnit(0.0254, 96.0)]
     public sealed class DeviceIndependentPixel : Unit
     {
-        /// <summary>
-        /// Returns a <see cref="System.String"/> that represents this instance.
-        /// </summary>
-        /// <returns>
-        /// A <see cref="System.String"/> that represents this instance.
-        /// </returns>
-        public override string ToString()
-        {
-            return "Device independent pixel (DIP)";
-        }
+        ///// <summary>
+        ///// Returns a <see cref="System.String"/> that represents this instance.
+        ///// </summary>
+        ///// <returns>
+        ///// A <see cref="System.String"/> that represents this instance.
+        ///// </returns>
+        //public override string ToString()
+        //{
+        //    return "Device independent pixel (DIP)";
+        //}
     }
 }
